1b1072ad79ac6170baf889355bf7b334952c9187
Mr-Rob0T-1 <harry.perros@gmail.com>
2026-06-29 05:14:32 +0300

    Changed variable names

--- 11888.py
+++ 11888.py
@@ -4,17 +4,19 @@ import requests
 import argparse
 
 from bs4 import BeautifulSoup as bsoup
-from time import sleep                                                                                    from contextlib import redirect_stdout                                                                    from rich.console import Console
+from time import sleep
+from contextlib import redirect_stdout
+from rich.console import Console
 
 parser = argparse.ArgumentParser()
-parser.add_argument("-n", "--name", help="victims last or full name.", type=str, required=True)
+parser.add_argument("-n", "--name", help="persons last or full name.", type=str, required=True)
 parser.add_argument("-o", "--outfile", help="writes results to results.txt", action="store_true")
 parser.add_argument("-s", "--sleep", help="adds delay between every get request crawl to reduce noise/rate limiting", type=float)
 args = parser.parse_args()
 
 def main(headers, rich_console):
 
-        victims_info = set()
+        persons_info = set()
         page_num = 1
 
         print("\n")
@@ -27,22 +29,23 @@ def main(headers, rich_console):
         while True:
 
                 try:
+
                         if args.sleep:
                                 delay = args.sleep
                         else:
                                 delay = 0
-                        vic_name = args.name
-                        vic_name = vic_name.replace(" ", "%20")
+                        person_name = args.name
+                        person_name = person_name.replace(" ", "%20")
                         query = f"https://www.11888.gr/white-pages/?query={vic_name}&amp%3Bpage=3&page={page_num}"
-                        vic_urls = set()
+                        person_urls = set()
 
                         # selects a random User agent from headers
                         ua = secrets.choice(range(1,len(headers) + 1))
                         header = {"User-Agent": f"{headers.get(ua)}"}
 
                         try:
-                            # this request returns the search results of the victim name
-                            html_content = session.get(query,timeout=4, headers=header)
+                            # this request returns the search results of the persons name
+                            html_content = session.get(query,timeout=1, headers=header)
                         except requests.Timeout:
                             print("GET Request for page timed out")
                             return
@@ -53,23 +56,23 @@ def main(headers, rich_console):
                         if html_content.status_code == 200:
                                 html_content = html_content.text
 
-                                #grabs all urls that lead to a victim
+                                #grabs all urls that lead to a person
                                 soup = bsoup(html_content, 'html.parser')
                                 for link in soup.find_all('a'):
                                         url = link.get('href')
                                         if url and 'white' in url and 'location' not in url and url != '/white-pages/':
-                                                vic_urls.add(url)
+                                                person_urls.add(url)
 
-                                # visits each victim url and grabs info
-                                for vic_url in vic_urls:
+                                # visits each persons url and grabs info
+                                for person_url in person_urls:
 
                                         ua = secrets.choice(range(1,len(headers) + 1))
                                         header = {"User-Agent": f"{headers.get(ua)}"}
 
-                                        vic_url = vic_url.lstrip("/")
+                                        person_url = person_url.lstrip("/")
 
                                         try:
-                                            victim_html = session.get(f"https://www.11888.gr/{vic_url}", headers=header, timeout=4)
+                                            person_html = session.get(f"https://www.11888.gr/{vic_url}", headers=header, timeout=4)
                                         except requests.Timeout:
                                             print("GET request timed out")
                                             return
@@ -77,14 +80,14 @@ def main(headers, rich_console):
                                             print(f"Request failed: {e}")
                                             return
 
-                                        vic_soup = bsoup(victim_html.text, "html.parser")
+                                        person_soup = bsoup(victim_html.text, "html.parser")
 
-                                        name = vic_soup.title.string.replace("| 11888.gr", "")
-                                        addr = vic_soup.select_one("span.tw-text-gray-secondary.tw-text-left.tw-text-sm.tw-select-none")
-                                        tel = re.search("tel:", victim_html.text)
+                                        name = person_soup.title.string.replace("| 11888.gr", "")
+                                        addr = person_soup.select_one("span.tw-text-gray-secondary.tw-text-left.tw-text-sm.tw-select-none")
+                                        tel = re.search("tel:", person_html.text)
 
                                         if tel:
-                                            tel = victim_html.text[tel.start():tel.start() + 14]
+                                            tel = person_html.text[tel.start():tel.start() + 14]
                                         else:
                                             tel = "tel:Not Found"
 
@@ -101,7 +104,7 @@ def main(headers, rich_console):
 
                                         rich_console.print("[bright_blue]|-----------------------------[/bright_blue]")
 
-                                        victims_info.add((name, addr, tel))
+                                        persons_info.add((name, addr, tel))
                                         sleep(delay)
                         else:
                             print("Didn't get 200 OK from site")
@@ -112,7 +115,7 @@ def main(headers, rich_console):
 
                 # page_num is incremented so recursion can happen.
                 page_num += 1
-                if vic_urls:
+                if person_urls:
                             continue
 
                 else:
@@ -121,7 +124,7 @@ def main(headers, rich_console):
                             rich_console.print("[bright_blue]|-----------------------------[/bright_blue]")
                             return
 
-                vic_urls = set()
+                person_urls = set()
 
 # the user agents that will be randomly selected each request.
 agents = {1:"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36",
